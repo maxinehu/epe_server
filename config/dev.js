546a09a14d31d62307110ad5caca0da121fe32cb
maxhubo <bohu@virtroid.com>
2018-03-02 00:13:54 -0800

    w

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-// dev keys, don't commit this
-
-module.exports = {
-  googleClientID: '772777108421-v1k8e58ahmd45mb6ffl5rdbjabcapf88.apps.googleusercontent.com',
-  googleClientSecret: '3K-5Ta1PXPUg7V4jK4ypaOHu',
-  mongoURI: 'mongodb://stephen:stephen@ds251518.mlab.com:51518/emaily-dev1',
-  cookieKey: 'ffwkehrkwrh23i4yjacmnvriey34235yuihnsakd',
-};
